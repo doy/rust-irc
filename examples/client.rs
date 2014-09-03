0d1ebb2da9e05966d6b3e6084186c65836b56668
Jesse Luehrs <doy@tozt.net>
2014-09-03 19:42:16 -0400

    write some login messages on connect in the example

--- examples/client.rs
+++ examples/client.rs
@@ -1,8 +1,31 @@
 extern crate irc;
 
+use irc::constants::{CommandMessage, Nick, User};
+
 fn main () {
     let mut client = irc::Client::new("doytest", "chat.freenode.net", 6667);
     client.connect();
+
+    client.write(
+        irc::Message::new(
+            None,
+            CommandMessage(Nick),
+            vec!["doytest".to_string()],
+        )
+    );
+    client.write(
+        irc::Message::new(
+            None,
+            CommandMessage(User),
+            vec![
+                "doytest".to_string(),
+                "localhost".to_string(),
+                "localhost".to_string(),
+                "doytest".to_string(),
+            ],
+        )
+    );
+
     loop {
         let res = client.read();
         println!("{}", res);
