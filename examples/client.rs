511b04bb455d483d69412501b98503797d79305b
Jesse Luehrs <doy@tozt.net>
2014-09-06 18:41:04 -0400

    have the example client respond to PING commands

--- examples/client.rs
+++ examples/client.rs
@@ -1,9 +1,17 @@
 extern crate irc;
 
+use irc::constants::{Ping, Pong};
+
 fn main () {
     let builder = irc::ClientBuilder::new("doytest", "chat.freenode.net");
     let client = builder.connect();
-    client.run_loop_with(|_client, m| {
+    client.run_loop_with(|client, m| {
         println!("{}", m);
+        match m.message_type() {
+            &Ping => {
+                client.write(irc::Message::new(None, Pong, m.params().clone()));
+            },
+            _ => {},
+        }
     });
 }
